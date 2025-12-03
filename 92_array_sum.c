bd6ba02d506d511b11d056e18eda193d093d6840
vivek yadav <vivekyadavcse24@gmail.com>
2025-12-03 17:46:48 +0530

    Auto update

---
+++ 92_array_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sumArray(int arr[], int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    return arr[n - 1] + sumArray(arr, n - 1);
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = sumArray(arr, n);
+    printf("Sum of array elements = %d\n", result);
+
+    return 0;
+}
